b19958905962531d626a056a51174e9f7b588c8e
Daniel Santos <contatodanieljob@gmail.com>
2022-03-03 11:56:03 -0300

    Adicionando Rota do Evento

---
+++ src/routes/RotaDoEvento/index.tsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import { createNativeStackNavigator } from '@react-navigation/native-stack'
+import { Events } from '../../screens/Events'
+import { TelaDoEvento } from '../../screens/TelaDoEvento'
+import { DadosDoEvento } from '../../components/CardEvento'
+
+export type TelasDaRotaDeEvento = {
+  Events: undefined,
+  TelaDoEvento: DadosDoEvento
+}
+
+const EventoStack = createNativeStackNavigator<TelasDaRotaDeEvento>()
+
+export const RotaDoEvento = () => {
+  return (
+    <EventoStack.Navigator 
+      initialRouteName='TelaDoEvento'
+      screenOptions={{headerShown: false}}
+    >
+      <EventoStack.Screen name='Events' component={Events}/>
+      <EventoStack.Screen name='TelaDoEvento' component={TelaDoEvento}/>
+    </EventoStack.Navigator>
+  )
+}
