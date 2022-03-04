23f122a54785c840fa44e6f4443da23f870a104a
AirtonM <93198566+AirtonM@users.noreply.github.com>
2022-03-04 10:07:54 -0300

    Alteração nas constantes do supabase

--- src/api/supabase.ts
+++ src/api/supabase.ts
@@ -1,4 +1,4 @@
 import { createClient } from "@supabase/supabase-js";
-import {SUPA_URL, SUPA_KEY} from '@env'
 
-export const supabase = createClient(SUPA_URL, SUPA_KEY)
+
+export const supabase = createClient('https://wamewmihjqjpmcfvjtzj.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsImlhdCI6MTYzNTQyMDIwMiwiZXhwIjoxOTUwOTk2MjAyfQ.-xblxgG9NeK16n1IDL6dVBtl-6jyOHG2H38h4YZMETI')
