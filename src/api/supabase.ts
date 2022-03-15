ff83bb90ef6c055083add0b786e5dced5926cd81
Daniel Santos <contatodanieljob@gmail.com>
2022-03-15 20:28:27 -0300

    corrigindo problema do LocalStorage

--- src/api/supabase.ts
+++ src/api/supabase.ts
@@ -1,4 +1,8 @@
 import { createClient } from "@supabase/supabase-js";
-import {SUPA_URL, SUPA_KEY} from '@env'
+import AsyncStorage from '@react-native-async-storage/async-storage'
+import { SUPA_URL, SUPA_KEY } from '@env'
 
-export const supabase = createClient(SUPA_URL, SUPA_KEY)
+export const supabase = createClient(SUPA_URL, SUPA_KEY, {
+  localStorage: AsyncStorage,
+  detectSessionInUrl: false
+})
