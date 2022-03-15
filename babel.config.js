ac159ee523ec12c578a3826ec86f99313dc14bdf
Daniel Santos <contatodanieljob@gmail.com>
2022-03-15 18:37:05 -0300

    adicionando RN Reanimated

--- babel.config.js
+++ babel.config.js
@@ -1,4 +1,4 @@
-module.exports = function(api) {
+module.exports = function (api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
@@ -10,7 +10,8 @@ module.exports = function(api) {
         "whitelist": null,
         "safe": false,
         "allowUndefined": true
-    }]
-   ]
+      }]
+      , 'react-native-reanimated/plugin'
+    ]
   };
 };
