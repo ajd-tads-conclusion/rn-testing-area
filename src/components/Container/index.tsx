2682e4c7a1cc829282e291660a7ffa791e392ba1
Daniel Santos <contatodanieljob@gmail.com>
2022-05-24 19:08:55 -0300

    feat: dinamically update width

--- src/components/Container/index.tsx
+++ src/components/Container/index.tsx
@@ -1,13 +1,14 @@
 import React from 'react'
-import { View, Dimensions } from 'react-native'
+import { View } from 'react-native'
 import { COLORS } from '../../theme/colors'
+import { useWindowDimensions } from 'react-native'
 
 type Props = {
   children: React.ReactNode
 }
 
 export const Container = (props: Props) => {
-  const { width } = Dimensions.get('screen')
+  const { width } = useWindowDimensions()
 
   return (
     <View
