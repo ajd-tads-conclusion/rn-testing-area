2b408ffea1989472f542b3a73d406da8a6ed537f
Daniel Santos <contatodanieljob@gmail.com>
2022-05-24 20:34:42 -0300

    feat: finish sign in screen

--- src/screens/SignIn/index.tsx
+++ src/screens/SignIn/index.tsx
@@ -1,14 +1,20 @@
 import * as yup from 'yup';
+import { COLORS } from '../../theme/colors';
 import { supabase } from '../../api/supabase'
 import React, { useEffect, useState } from 'react';
 import { useForm, Controller } from 'react-hook-form';
 import { yupResolver } from '@hookform/resolvers/yup';
 import type { TelasDaRotaAuth } from '../../routes/Auth';
+import { LabelDeErro } from '../../components/LabelDeErro';
 import { useToast } from 'react-native-toast-notifications'
-import { TextInput, View, Text, Button } from 'react-native';
+import { TextInput, View, Text, Pressable } from 'react-native';
 import type { NativeStackScreenProps } from '@react-navigation/native-stack';
 import { AuthResponse, logarUsuario } from '../../routes/Auth/supabaseAuth';
-import { checarSessaoLocalmente, removeSessaoLocalmente, salvaSessaoLocalmente } from '../../helpers/AsyncStorage/asyncStorage';
+import {
+  checarSessaoLocalmente,
+  removeSessaoLocalmente,
+  salvaSessaoLocalmente
+} from '../../helpers/AsyncStorage/asyncStorage';
 
 type Props = NativeStackScreenProps<TelasDaRotaAuth, 'SignIn'>
 
@@ -24,6 +30,7 @@ const schema = yup.object({
 
 export const SignIn = ({ navigation }: Props) => {
   const [respostaDoSupabase, setRespostaDoSupabase] = useState<AuthResponse>({ user: null, error: null })
+  const [loading, setLoading] = useState(false)
   const toast = useToast()
 
   const { control, handleSubmit } = useForm<Inputs>({
@@ -72,14 +79,43 @@ export const SignIn = ({ navigation }: Props) => {
   }, [respostaDoSupabase])
 
   const onSubmit = async (data: Inputs) => {
+    setLoading(true)
+
     const { error, user } = await logarUsuario(data.email, data.password)
+
     setRespostaDoSupabase({ user, error })
+    setLoading(false)
   }
 
   return (
-    <View>
+    <View
+      style={{
+        flex: 1,
+      }}
+    >
 
-      <View >
+      <View
+        style={{
+          flex: 1,
+          margin: 10,
+          padding: 10,
+          borderRadius: 5,
+          backgroundColor: COLORS.secondary,
+          borderColor: COLORS.white,
+          borderWidth: 1
+        }}
+      >
+
+        <Text
+          style={{
+            fontSize: 20,
+            fontWeight: 'bold',
+            color: COLORS.white,
+            marginBottom: 10
+          }}
+        >
+          E-mail:
+        </Text>
 
         <Controller
           control={control}
@@ -91,12 +127,37 @@ export const SignIn = ({ navigation }: Props) => {
                   placeholder='E-mail'
                   value={value}
                   onChangeText={onChange}
+                  placeholderTextColor={COLORS.white + '9C'}
+                  style={{
+                    backgroundColor: COLORS.tertiary,
+                    borderColor: errors.email && COLORS.error,
+                    borderWidth: errors.email && 1,
+                    borderRadius: 5,
+                    marginBottom: 10,
+                    padding: 10,
+                    color: errors.email ? COLORS.error : COLORS.white,
+                    fontWeight: 'bold'
+                  }}
                 />
-                {errors.email && <Text >{errors.email.message}</Text>}
+                {
+                  errors.email && <LabelDeErro campo={errors.email} />
+                }
               </>
             )
           }}
         />
+
+        <Text
+          style={{
+            fontSize: 20,
+            fontWeight: 'bold',
+            color: COLORS.white,
+            marginBottom: 10
+          }}
+        >
+          Senha:
+        </Text>
+
         <Controller
           control={control}
           name={'password'}
@@ -107,25 +168,74 @@ export const SignIn = ({ navigation }: Props) => {
                   placeholder='Senha'
                   value={value}
                   onChangeText={onChange}
+                  placeholderTextColor={COLORS.white + '9C'}
+                  secureTextEntry={true}
+                  style={{
+                    backgroundColor: COLORS.tertiary,
+                    borderColor: errors.password && COLORS.error,
+                    borderWidth: errors.password && 1,
+                    borderRadius: 5,
+                    marginBottom: 10,
+                    padding: 10,
+                    color: errors.password ? COLORS.error : COLORS.white,
+                    fontWeight: 'bold'
+                  }}
                 />
-                {errors.password && <Text >{errors.password.message}</Text>}
+
+                {
+                  errors.password && <LabelDeErro campo={errors.password} />
+                }
               </>
             )
           }}
         />
-        <Button
+        <Pressable
+          disabled={loading}
           onPress={handleSubmit(onSubmit)}
-          title='Entrar'
-        />
+          style={{
+            backgroundColor: loading ? COLORS.secondary : COLORS.primary,
+            borderRadius: 5,
+            padding: 10
+          }}
+        >
+          <Text
+            style={{
+              color: COLORS.white,
+              fontSize: 15,
+              fontWeight: 'bold',
+              textAlign: 'center'
+            }}
+          >
+            Entrar
+          </Text>
+        </Pressable>
 
         <View>
-          <Text>
+          <Text
+            style={{
+              fontSize: 15,
+              fontWeight: 'bold',
+              color: COLORS.white,
+              marginTop: 10,
+              textAlign: 'center'
+            }}
+          >
             Não possui uma conta?
+            <Pressable
+              onPress={() => navigation.navigate('SignUp')}
+            >
+              <Text
+                style={{
+                  padding: 5,
+                  textDecorationLine: 'underline',
+                  textDecorationStyle: 'solid',
+                }}
+              >
+                Cadastre-se
+              </Text>
+            </Pressable>
+
           </Text>
-          <Button
-            title='Cadastre-se'
-            onPress={() => navigation.navigate('SignUp')}
-          />
         </View>
 
       </View>
